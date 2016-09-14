fb64151ae4b711939ea3cc11e75dcc99130da21e
Nhan Cao <nhancv1992@gmail.com>
2016-09-14 16:45:16 +0700

    change port: one2one: 7001 one2many: 7000

--- kurento-one2one-call/server.js
+++ kurento-one2one-call/server.js
@@ -26,7 +26,7 @@ var https = require('https');
 
 var argv = minimist(process.argv.slice(2), {
   default: {
-      as_uri: "https://localhost:8443/",
+      as_uri: "https://localhost:7001/",
       ws_uri: "ws://localhost:8888/kurento"
   }
 });
